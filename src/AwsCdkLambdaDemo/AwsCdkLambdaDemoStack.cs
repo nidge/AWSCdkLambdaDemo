02d502f2f0400683b7f0eadc3f57892b573c9269
Nigel <nigelsmyth@hotmail.com>
2022-05-20 15:09:10 +0100

    Initial commit

---
+++ src/AwsCdkLambdaDemo/AwsCdkLambdaDemoStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace AwsCdkLambdaDemo
+{
+    public class AwsCdkLambdaDemoStack : Stack
+    {
+        internal AwsCdkLambdaDemoStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
